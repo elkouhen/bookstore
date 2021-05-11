26a0160ff60fe03ad56a7c116703fd55ca1b5f77
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2021-05-11 14:41:06 +0200

    WIP

--- bookstore-api/target/classes/db/migration/R__insert_data.sql
+++
@@ -1,2 +0,0 @@
-insert into BOOK (id, author, title) values (1, 'GoF', 'Design Patterns') on conflict (id) do nothing;
-insert into BOOK (id, author, title) values (2, 'C', 'Kernighan') on conflict (id) do nothing;
